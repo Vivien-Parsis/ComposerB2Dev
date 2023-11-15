9edb04d5a652e097c9e54d77c250e5ff51fb1798
VIvien-Parsis <vivienparsis.perso@gmail.com>
2023-11-15 18:37:48 +0100

    init

---
+++ Readme.md
@@ -0,0 +1,2 @@
+to test <br>
+php bin/console composer:validate test/composer.json --lang=en
---
+++ src/Validator/ComposerValidator.php
@@ -0,0 +1,43 @@
+<?php 
+    namespace App\Validator;
+    use App\Log\FooLog;
+    
+    class ComposerValidator
+    {
+        public static function validate(string $composerPath, string $lang = 'en'): array
+        {
+            // Lire le contenu du fichier composer.json
+            $composerContent = json_decode(file_get_contents($composerPath), true);
+            if ($composerContent === null) {
+                throw new \InvalidArgumentException('composer.json is empty or invalid JSON.');
+            }
+            // Valider le nom, la description, require et version
+            $nameValid = self::validateName($composerContent['name']);
+            $descriptionValid = self::validateDescription($composerContent['description']);
+            $requireValid = self::validateRequire($composerContent['require']);
+            $versionValid = self::validateVersion($composerContent['require']['php']);
+            return [
+                'name' => $nameValid,
+                'description' => $descriptionValid,
+                'require' => $requireValid,
+                'version' => $versionValid,
+            ];
+        }
+    
+        private static function validateName(string $name): bool{
+            return !empty($name);
+        }
+    
+        private static function validateDescription(string $description): bool{
+            return !empty($description);
+        }
+    
+        private static function validateRequire(array $require): bool{
+            return !empty($require);
+        }
+    
+        private static function validateVersion(string $version): bool{
+            return !empty($version);
+        }
+    }
+?>
