00e6c8b8a3bd41c59096810d7d06011ff8ba06fc
VIvien-Parsis <vivienparsis.perso@gmail.com>
2023-11-15 15:45:47 +0100

    change to symfony project

--- src/maker/makeLog.php
+++ src/maker/makeLog.php
@@ -2,13 +2,13 @@
 
     namespace App\makeLog;
 
-use Symfony\Bundle\MakerBundle\ConsoleStyle;
-use Symfony\Bundle\MakerBundle\Generator as MakerBundleGenerator;
-use Symfony\Bundle\MakerBundle\InputConfiguration;
-use Symfony\Bundle\MakerBundle\Maker\AbstractMaker;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Bundle\MakerBundle\DependencyBuilder;
+    use Symfony\Bundle\MakerBundle\ConsoleStyle;
+    use Symfony\Bundle\MakerBundle\Generator as MakerBundleGenerator;
+    use Symfony\Bundle\MakerBundle\InputConfiguration;
+    use Symfony\Bundle\MakerBundle\Maker\AbstractMaker;
+    use Symfony\Component\Console\Input\InputInterface;
+    use Symfony\Component\Console\Command\Command;
+    use Symfony\Bundle\MakerBundle\DependencyBuilder;
 
     final class makeLog extends AbstractMaker{
         public static function getCommandName():string{
